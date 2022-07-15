230cc893b87be133b3fa704adb2bdcd567090f26
Igor Chichinin <marafon4k@gmail.com>
2022-07-15 22:33:17 +0300

    task_02 solution_1

--- task_02/Program.cs
+++ task_02/Program.cs
@@ -5,25 +5,27 @@
 // A (3,6,8); B (2,1,-7), -> 15.84
 // A (7,-5, 0); B (1,-1,9) -> 11.53
 
+Console.Clear();
+
 Console.WriteLine("Enter point X1: ");
 int x1 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Enter point Y1: ");
 int y1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter point Z1: ");
+int z1 = Convert.ToInt32(Console.ReadLine());
 
 Console.WriteLine("Enter point X2: ");
 int x2 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Enter point Y2: ");
 int y2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter point Z2: ");
+int z2 = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("Enter point X3: ");
-int x3 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Enter point Y3: ");
-int y3 = Convert.ToInt32(Console.ReadLine());
-
-double LongXY(int x1n, int y1n, int x2n, int y2n, int x3n, int y3n)
+double LongXY(int x1n, int y1n, int z1n, int x2n, int y2n, int z2n)
 {
-    // return Math.Sqrt((x1n - x2n) * (x1n - x2n) + (y1n - y2n) * (y1n - y2n));
+    return Math.Sqrt((x1n - x2n) * (x1n - x2n) + (y1n - y2n) * (y1n - y2n) + (z1n - z2n) * (z1n - z2n));
+    // return Math.Sqrt((x1n - x2n) * (x1n - x2n) + (y1n - y2n) * (y1n - y2n) + (z1n - z1n) * (z1n - z1n));
 }
 
-double result = LongXY(x1, y1, x2, y2);
+double result = LongXY(x1, y1, z1, x2, y2, z2);
 Console.WriteLine(Math.Round(result, 2));
