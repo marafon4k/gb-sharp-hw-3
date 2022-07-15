0f50eaa2515c1fd2eb2b1b3ecc21fdb6e5f65db9
Igor Chichinin <marafon4k@gmail.com>
2022-07-15 22:16:22 +0300

    First commit

---
+++ task_01/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.Clear();
+
+// Задача 19
+// Напишите программу, которая принимает на вход 
+// пятизначное число и проверяет, является ли оно палиндромом.
+
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+// Console.Write("Введите число:");
+// int n = Convert.ToInt32(Console.ReadLine());
+
+// WriteSquareTable(n);
+
+// void WriteSquareTable(int n)
+// {
+//     int i = 1;
+
+//     while (i <= n)
+//     {
+//         Console.WriteLine($"| {i} | {i * i,2} |");
+//         i++;
+//     }
+// }
---
+++ task_02/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 21
+// Напишите программу, которая принимает на вход 
+// координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+Console.WriteLine("Enter point X1: ");
+int x1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter point Y1: ");
+int y1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter point X2: ");
+int x2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter point Y2: ");
+int y2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter point X3: ");
+int x3 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter point Y3: ");
+int y3 = Convert.ToInt32(Console.ReadLine());
+
+double LongXY(int x1n, int y1n, int x2n, int y2n, int x3n, int y3n)
+{
+    // return Math.Sqrt((x1n - x2n) * (x1n - x2n) + (y1n - y2n) * (y1n - y2n));
+}
+
+double result = LongXY(x1, y1, x2, y2);
+Console.WriteLine(Math.Round(result, 2));
