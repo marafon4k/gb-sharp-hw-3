f74564e8f9bbd01f811c2005c89bcf8b956672f8
Igor Chichinin <marafon4k@gmail.com>
2022-07-17 02:33:52 +0300

    task_01 solutions 1 added

--- task_01/Program.cs
+++ task_01/Program.cs
@@ -4,22 +4,33 @@
 // Напишите программу, которая принимает на вход 
 // пятизначное число и проверяет, является ли оно палиндромом.
 
-// 14212 -> нет
-// 12821 -> да
-// 23432 -> да
+// 14212 -> No
+// 12821 -> Yes
+// 23432 -> Yes
 
-// Console.Write("Введите число:");
-// int n = Convert.ToInt32(Console.ReadLine());
+void Poly()
+{
+    int i = 1;
+    while (i == 1)
+    {
+        Console.WriteLine("Enter number");
+        if (int.TryParse(Console.ReadLine(), out var num))
+        {
+            if (num == 1) break;
 
-// WriteSquareTable(n);
+            int dig = 0,
+                rev = 0,
+                numb = num;
+            while (num > 0)
+            {
+                dig = num % 10; // 14212 2 1 2 4 1 stop
+                rev = rev * 10 + dig; // 2 21 212 2124 21241
+                num = num / 10; // 14212 1421 142 14 1 0
+            }
+            if (numb == rev) Console.WriteLine("Yes"); else Console.WriteLine("No");
+            Console.WriteLine("Write 1 if you want to stop");
+        }
+    }
+}
 
-// void WriteSquareTable(int n)
-// {
-//     int i = 1;
-
-//     while (i <= n)
-//     {
-//         Console.WriteLine($"| {i} | {i * i,2} |");
-//         i++;
-//     }
-// }
+Poly();
