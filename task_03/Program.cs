3ffd6232758311df96a0fb6dd15146f895ee6c98
Igor Chichinin <marafon4k@gmail.com>
2022-07-17 10:52:28 +0300

    task_03 solution 1 added

--- task_03/Program.cs
+++ task_03/Program.cs
@@ -3,3 +3,31 @@
 
 // 3 -> 1, 8, 27
 // 5 -> 1, 8, 27, 64, 125
+
+Console.Clear();
+
+void Pow()
+{
+    int i = 0;
+    while (i == 0)
+    {
+        Console.WriteLine("Enter number");
+        if (int.TryParse(Console.ReadLine(), out var number))
+        {
+            if (number == 1) break;
+
+            double hoarder = 0;
+
+            while (hoarder < number)
+            {
+                double dev = number;
+                hoarder++;
+                dev = Math.Pow(hoarder, 3);
+                Console.WriteLine($" | {dev} | ");
+            }
+            Console.WriteLine("Write 1 if you want to stop");
+        }
+    }
+}
+
+Pow();
